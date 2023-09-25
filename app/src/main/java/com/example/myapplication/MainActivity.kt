078b6c28adb65839e8f39497dc82a4e9fd76df14
CaioPS <caio.silva8@alunos.unis.edu.br>
2023-09-24 21:43:24 -0300

    Adicionado intent e view binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+
+        main.login.setOnClickListener() {
+            val goToShop = Intent(this, ShopActivity::class.java)
+            startActivity(goToShop)
+        }
     }
 }
